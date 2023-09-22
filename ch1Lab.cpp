c0d817e8a5966fd6746ba85008281bd7d23c54fe
Ian Luna Quiroz <ianmlunaq@gmail.com>
2023-09-21 22:51:08 -0700

    A

--- ch1Lab.cpp
+++ ch1Lab.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 const int NUM_ROWS = 3;
 const int NUM_COLUMNS = 3;
+const int MAX_TURNS = 9;
 
 class TicTacToeBoard {
     private:
@@ -14,23 +15,57 @@ class TicTacToeBoard {
     public:
         TicTacToeBoard();
         int printBoard();
+        int xPlay();
+        int oPlay();
+        int determineWinner();
 };
 
 TicTacToeBoard::TicTacToeBoard() {
     for(int x = 0; x < NUM_ROWS; x++) {
-        for(int y = 0; y < 0; y++) {
+        for(int y = 0; y < NUM_COLUMNS; y++) {
             gameBoard[x][y] = '*';
         }
     }
 }
 
 int TicTacToeBoard::printBoard() {
+    cout << "   1  2  3\n";
     for(int x = 0; x < NUM_ROWS; x++) {
-        for(int y = 0; y < 0; y++) {
+        cout << x + 1 << " ";
+        for(int y = 0; y < NUM_COLUMNS; y++) {
             cout << "[" << gameBoard[x][y] << "]";
         }
+        cout << endl;
     }
 
+    return 0;
+}
+
+int TicTacToeBoard::xPlay() {
+    int x, y;
+
+    cout << "It is X player\'s turn.\nType in row...";
+    cin >> x;
+    cout << "\nType in column";
+    cin >> y;
+
+    gameBoard[x][y] = 'X';
+    return 0;
+}
+
+int TicTacToeBoard::oPlay() {
+    int x, y;
+
+    cout << "It is O player\'s turn.\nType in row...";
+    cin >> x;
+    cout << "\nType in column";
+    cin >> y;
+
+    gameBoard[x][y] = 'O';
+    return 0;
+}
+
+int TicTacToeBoard::determineWinner() {
     return 0;
 }
 
